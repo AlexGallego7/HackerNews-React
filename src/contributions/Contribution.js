7f980dea6a0aba06b431e868ada7e3c03cbcf93c
Alex Gallego <alex.gallego9@gmail.com>
2020-12-08 15:09:23 +0100

    first commit v2

--- src/contributions/Contribution.js
+++ src/contributions/Contribution.js
@@ -3,13 +3,32 @@ import ContributionView from "./ContributionView";
 
 class Contribution extends React.Component {
 
+    /*
+        Utilizaremos un constructor aqui que tenga un parametro
+        para definir el tipo de contribuciones que queremos (ask, all, url).
+        Haremos un set del state con el tipo y haremos un if en el
+        componentDidMount para la url y el fetch
+    */
+
     state = {
         error: null,
+        type: null,
         contributions: []
     }
 
     componentDidMount() {
-        fetch("https://asw-hackernews-kaai12.herokuapp.com/api/contributions")
+
+        /*
+        if (this.status.type == 'all')
+            url = "https://asw-hackernews-kaai12.herokuapp.com/api/newest"
+        else if (this.status.type == 'ask')
+            url = "https://asw-hackernews-kaai12.herokuapp.com/api/ask"
+        else if (this.status.type == 'url')
+            url = "https://asw-hackernews-kaai12.herokuapp.com/api/contributions"
+         */
+
+        let url = "https://asw-hackernews-kaai12.herokuapp.com/api/contributions"
+        fetch(url)
             .then(response => response.json())
             .then(
                 (result) => {
--- src/contributions/ContributionView.js
+++ src/contributions/ContributionView.js
@@ -1,6 +1,6 @@
 const ContributionView = (props) => (
     <div class="content">
-        <ol cls="inline gap">
+        <ol class="inline gap">
             {props.contributions.map( (contribution) =>
                     <div>{contribution.title + ' (' +
                     contribution.url + ')'}</div>
